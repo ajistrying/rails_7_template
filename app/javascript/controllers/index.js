29f214238c3cb8081bfb1629b311b7d8f90b3da0
Wellington Johnson <ajistrying@gmail.com>
2023-03-01 23:50:46 -0500

    prettier linting and more pricing page work, need to hook up stripe subscriptions next

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,18 +2,27 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "./application";
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import HelloController from "./hello_controller";
+application.register("hello", HelloController);
 
 // Import and register all TailwindCSS Components
-import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
-application.register('alert', Alert)
-application.register('autosave', Autosave)
-application.register('dropdown', Dropdown)
-application.register('modal', Modal)
-application.register('tabs', Tabs)
-application.register('popover', Popover)
-application.register('toggle', Toggle)
-application.register('slideover', Slideover)
+import {
+  Alert,
+  Autosave,
+  Dropdown,
+  Modal,
+  Tabs,
+  Popover,
+  Toggle,
+  Slideover,
+} from "tailwindcss-stimulus-components"
+application.register("alert", Alert);
+application.register("autosave", Autosave);
+application.register("dropdown", Dropdown);
+application.register("modal", Modal);
+application.register("tabs", Tabs);
+application.register("popover", Popover);
+application.register("toggle", Toggle);
+application.register("slideover", Slideover);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 module.exports = {
-  plugins: [
-    require('@tailwindcss/forms'),
-    require("daisyui"),
-  ],
+  plugins: [require("@tailwindcss/forms"), require("daisyui")],
   daisyui: {
     themes: ["winter"],
   },
   content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
-}
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js",
+  ],
+};
