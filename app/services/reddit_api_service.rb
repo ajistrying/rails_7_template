45441829c3d40fa00e75ad7558696d32e4ae9649
Wellington Johnson <ajistrying@gmail.com>
2023-04-28 00:52:48 -0400

    all API endpoints so far"

--- app/services/reddit_api_service.rb
+++ app/services/reddit_api_service.rb
@@ -4,10 +4,27 @@ require 'json'
 
 class RedditApiService
     class << self
-        def get_hot_posts
+        def get_hot_posts_from(subreddit)
             make_api_call do |conn|
-                subreddit = "blackpeopletwitter"
                 response = conn.get("/hot", {name: subreddit})
+                data = JSON.parse(response.body)
+                puts data
+            end
+        end
+
+        def search_subreddit(subreddit, query)
+            make_api_call do |conn|
+                response = conn.post("/search") do |req|
+                    req.body = {name: subreddit, query: query}.to_json
+                end
+                data = JSON.parse(response.body)
+                puts data
+            end
+        end
+
+        def get_trending_subreddits
+            make_api_call do |conn|
+                response = conn.get("/trending")
                 data = JSON.parse(response.body)
                 puts data
             end
@@ -15,7 +32,7 @@ class RedditApiService
 
         private
 
-        def make_api_call(url, params)
+        def make_api_call
             bearer_token = "4qsspcwhycqta5b6h4wc7pxtyg4qbw9fhca"
 
             conn = Faraday.new(
